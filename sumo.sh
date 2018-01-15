546e939d49dea65f72b1d6952d34852e77bbe711
wraith996 <35116975+wraith996@users.noreply.github.com>
2018-01-15 20:05:30 +0100

    Update sumo.sh

--- sumo.sh
+++ sumo.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-printf "monero\nsumo.bohemianpool.com:8888\nSumoo6Tk19oFWeUrDon3XTRhAPtZtEFcBXtF33mznZL3VykBZEWT5ZxV9z1v3hfJEUbQidV7H55ox1v5coUk79pNTgsbZo19P8s\nd\nn\nn\nn\n" | /opt/xmr-stak/build/bin/xmr-stak
+printf "monero\nsumo.bohemianpool.com:8888\nSumoo55b63UXWcrec9QaVFCuJUQdbStaW7C3kVFb6vhUjPspud5fa6ZgqXjnsMkx7CWuHvZRogpnD6owcBxpBzMMU24XX4LDDGf\np\nn\nn\nn\n" | /opt/xmr-stak/build/bin/xmr-stak
