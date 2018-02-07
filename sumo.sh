3b7f4200d284a26abb95c10efafa81e80fcfdf00
wraith996 <35116975+wraith996@users.noreply.github.com>
2018-02-07 16:14:39 +0100

    Update sumo.sh

--- sumo.sh
+++ sumo.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-printf "monero\npool.sumokoin.hashvault.pro:3333\nSumoo6Tk19oFWeUrDon3XTRhAPtZtEFcBXtF33mznZL3VykBZEWT5ZxV9z1v3hfJEUbQidV7H55ox1v5coUk79pNTgsbZo19P8s\np\nn\nn\nn\n" | /opt/xmr-stak/build/bin/xmr-stak
+printf "monero\nsumo.kryptopond.net:3333\nSumoo6Tk19oFWeUrDon3XTRhAPtZtEFcBXtF33mznZL3VykBZEWT5ZxV9z1v3hfJEUbQidV7H55ox1v5coUk79pNTgsbZo19P8s\np\nn\nn\nn\n" | /opt/xmr-stak/build/bin/xmr-stak
